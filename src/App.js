036584e3d7c502b396e0350945fda2f64ad4b3d3
shakhbozbekusmonov <miracle_0319@bk.ru>
2022-02-23 08:31:07 +0500

    Created Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import {Header} from "./components/header/Header";
+import {Footer} from "./components/footer/Footer";
+import {Route, Switch} from "react-router-dom";
+import {Home} from "./pages/Home";
+import {About} from "./pages/About";
+import {Contact} from "./pages/Contact";
+import {NotFound} from "./pages/NotFound";
+import './main.scss';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Header/>
+      <main className="container content">
+          <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/about" exact component={About}/>
+              <Route path="/contact" exact component={Contact}/>
+              <Route component={NotFound}/>
+          </Switch>
+      </main>
+      <Footer/>
+    </>
   );
 }
 
