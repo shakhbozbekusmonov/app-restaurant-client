637b0f44b4d756fd4a76ed1735504f8b19e6aca0
shakhbozbekusmonov <miracle_0319@bk.ru>
2022-02-26 20:44:42 +0500

    Created Search

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -3,12 +3,12 @@ import {CategoryItem} from "./CategoryItem";
 
 export const CategoryList = ({catalog = []}) => {
     return (
-        <div className="container">
+        <>
             <div className="row">
                 {catalog.map(item => (
                     <CategoryItem key={item.idCategory} {...item}/>
                 ))}
             </div>
-        </div>
+        </>
     );
 };
